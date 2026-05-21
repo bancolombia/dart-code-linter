d2fbbc11152c38658b88f3c78467208f666b46d4
Pavel Akhrameev <P.akhrameev@gmail.com>
2026-05-21 17:07:41 +0400

    fix(metrics): respect per-line `// ignore:` comments for metric violations (#236)
    * fix(metrics): respect per-line `// ignore:` comments for metric violations
    
    Function- and class-level metrics now honor inline `// ignore: <metric-id>`
    comments (both above the declaration and trailing on the same line),
    matching the behavior already in place for rules. File metrics keep their
    file-level-only suppression.
    
    * chore: bump version to 4.0.4 and update CHANGELOG for inline ignore
    
    * update readme as well

---
+++ test/resources/metric_suppression/inline_class_suppression_example.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: no-empty-block
+
+// ignore: number-of-methods
+class SuppressedAbove {
+  void m1() {}
+  void m2() {}
+  void m3() {}
+  void m4() {}
+  void m5() {}
+}
+
+class SuppressedTrailing { // ignore: number-of-methods
+  void m1() {}
+  void m2() {}
+  void m3() {}
+  void m4() {}
+  void m5() {}
+}
+
+class NotSuppressed {
+  void m1() {}
+  void m2() {}
+  void m3() {}
+  void m4() {}
+  void m5() {}
+}
